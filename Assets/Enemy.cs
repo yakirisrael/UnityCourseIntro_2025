ec5615032a6015dc4cde6f5ec87cf1bf6598817e
MystDruid <yakir.israel1@gmail.com>
2025-08-07 15:38:25 +0300

    Added GetObjectWith Tag, Coroutine for Enemy

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,5 +1,15 @@
 using System;
+using System.Collections;
 using UnityEngine;
+
+
+enum EnemyState
+{
+    None,
+    ChasePlayer,
+    Attack,
+    WaitForAttack
+}
 
 public class Enemy : MonoBehaviour
 {
@@ -7,10 +17,21 @@ public class Enemy : MonoBehaviour
     public int speed = 1;
 
     public bool inZone = false;
+
+    private EnemyState state = EnemyState.None;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
     private void Update()
     {
         if (inZone == false)
         {
+            // chase after player
+            state = EnemyState.ChasePlayer;
+            
             Vector3 distance = target.transform.position - transform.position;
 
             transform.position += distance.normalized * (speed * Time.deltaTime);
@@ -22,7 +43,7 @@ public class Enemy : MonoBehaviour
                     transform.localScale.y,
                     transform.localScale.z);
             }
-            else
+            else 
             {
                 transform.localScale = new Vector3(
                     Vector3.right.x * Mathf.Abs(transform.localScale.x),
@@ -30,7 +51,24 @@ public class Enemy : MonoBehaviour
                     transform.localScale.z);
             }
         }
+        else if (state == EnemyState.ChasePlayer) // reach to the player
+        {
+            StartCoroutine(AttackAndWaitLoop());
+        }
         
-       
+    }
+
+    IEnumerator AttackAndWaitLoop()
+    {
+        while (true)
+        {
+            state = EnemyState.Attack;
+            Debug.Log("Attacking the player");
+            yield return new WaitForSeconds(2.0f);
+
+            state = EnemyState.WaitForAttack;
+            Debug.Log("Waiting....");
+            yield return new WaitForSeconds(2.0f);
+        }
     }
 }
